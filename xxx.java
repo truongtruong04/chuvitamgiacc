775c39b976fe1cac40b4353c7ed0d835fa91be6b
truongkhongsai <anhp31746@gmail.com>
2023-04-29 00:10:23 +0700

    fist commit

---
+++ xxx.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class xxx {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập vào các cạnh của tam giác :");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int i = (a + b + c);
+        System.out.println(" chu vi của tam giác là :" + i) ;
+
+    }
+}
+
